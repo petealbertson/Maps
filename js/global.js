40acd8a30c88e1aafc11690c9d95a9ab559eb756
Pete Albertson <albertson.peter@gmail.com>
2014-02-24 12:05:58 -0600

    initial update

--- js/global.js
+++ js/global.js
@@ -1,14 +1,25 @@
 // Load the following when page is ready (sans jQuery)
 document.addEventListener('DOMContentLoaded', function(){
     var mapID = "map";
-    var mapBoxID = "examples.map-9ijuk24y";
+    var mapBoxID = "petealbertson.hc43hj5e";
 
     var startingLatLong = [41.888569, -87.635528];
-    var startingZoom = 13;
-
+    var startingZoom = 14;
+    var myGeoControl = L.mapbox.geocoderControl(mapBoxID);
+    window.data = [];
     // Initialize the map.
     var map = L.mapbox.map(mapID, mapBoxID)
-        .setView(startingLatLong, startingZoom);
+        .setView(startingLatLong, startingZoom)
+        .addControl(myGeoControl);
+
+        myGeoControl.on('found', function(theResult){
+            var newList = _.map(data, function(object, key){
+                object['distance'] = window.distance(theResult.latlng, [object.latitude, object.longitude]);
+                return object;
+            });
+            var closestList = _.sortBy(newList, function(object) { return object.distance; });
+            alert("You are closest to " + closestList[0].location + "! Get some veggies.");
+        });
 
     // Get JSON data from Data Portal. (The following is instead of $.getJSON)
     // Source: https://data.cityofchicago.org/Environment-Sustainable-Development/Farmers-Markets-2013/i8y3-ytj4
@@ -19,10 +30,36 @@ document.addEventListener('DOMContentLoaded', function(){
     request.onload = function() {
       if (request.status >= 200 && request.status < 400){
         // Success!
-        var data = JSON.parse(request.responseText);
+         data = JSON.parse(request.responseText);
 
         // Loop through the data
             // … and update the map
+        var points = []
+        _.each(data, function(element, index, list) {
+            // console.log(element)
+            var lat = element['latitude']
+            var lon = element['longitude']
+            L.mapbox.featureLayer({
+            // this feature is in the GeoJSON format: see geojson.org
+            // for the full specification
+            type: 'Feature',
+            geometry: {
+            type: 'Point',
+            // coordinates here are in longitude, latitude order because
+            // x, y is the standard for GeoJSON and many formats
+            coordinates: [lon, lat]
+            },
+            properties: {
+            title: element['location'],
+            description: element.intersection,
+            // one can customize markers by adding simplestyle properties
+            // http://mapbox.com/developers/simplestyle/
+            'marker-size': 'large',
+            'marker-color': '#f0a'
+            }
+
+            }).addTo(map);
+        });
 
       } else {
         // We reached our target server, but it returned an error
